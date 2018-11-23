59b85ebb76ab41b205a285c521b13073e59bd3fb
Timmy Beatty <tim.beatty@gmail.com>
2018-11-23 13:50:50 -0800

    Changed algorithm to find prime numbers. Should be more efficient, but not efficient enough. I think it's a memory problem. 200k rows takes 30 seconds (still too slow), but 400k rows takes 254 seconds. So, definitely not linear.

--- problem_10.py
+++ problem_10.py
@@ -1,32 +1,60 @@
-'''
+"""
+PROBLEM:
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
-'''
-from math import sqrt
+"""
 
 
-def is_prime(number):
-    if number == 1:
-        return False
-    elif number == 2:
-        return True
-    elif number % 2 == 0:
-        return False
+"""
+RESEARCH: 
+In mathematics, the sieve of Eratosthenes is a simple, ancient algorithm for finding all prime numbers up to any given 
+limit.
+
+It does so by iteratively marking as composite (i.e., not prime) the multiples of each prime, starting with the 
+first prime number, 2. The multiples of a given prime are generated as a sequence of numbers starting from that 
+prime, with constant difference between them that is equal to that prime.[1] This is the sieve's key distinction 
+from using trial division to sequentially test each candidate number for divisibility by each prime.[2]
+
+"""
+# 20,000 rows = 0.33 seconds
+# 200,000 rows = 32.6 seconds
+# 400,000 rows = 254 seconds...
+
+import time
+
+start = time.time()
+
+below_num = 400000
+nums_to_check = range(2, below_num)
+max_to_check = max(nums_to_check)
+cur_num = min(nums_to_check)
+
+prime_list = []
+total = 0
+
+while cur_num <= max_to_check:
+
+    # print("To Check Pre {}".format(nums_to_check))
+
+    # Add number to prime list
+    prime_list.append(cur_num)
+    # Create list of all multiples of current number
+    more_not_prime = list(range(cur_num, below_num, cur_num))
+    # print("More Not Prime = {}".format(more_not_prime))
+    # Remove multiples of current number from numbers to check
+    nums_to_check = list(set(nums_to_check) - set(more_not_prime))
+    # print("To Check Post {}".format(nums_to_check))
+
+    if not nums_to_check:
+        break
     else:
-        for i in range(2, int((number - 1) / 2)):
-            if number % i == 0:
-                return False
+        max_to_check = max(nums_to_check)
+        cur_num = min(nums_to_check)
+        # print(max_to_check)
+        # print(cur_num)
 
-        return True
+end = time.time()
 
-
-# Input
-below_num = 2000000
-total = 2
-
-for num in range(3, below_num, 2):
-    if is_prime(num):
-        total += num
-
-print(total)
+print(end - start)
+print(sum(prime_list))
